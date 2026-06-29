8807c2c1babe92b3a21870608fafa42858338d11
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2026-06-29 12:23:03 +0200

    chore: migrate to Junit 5 (from openrewrite)

--- src/test/java/org/esupportail/activbo/UtilsTest.java
+++ src/test/java/org/esupportail/activbo/UtilsTest.java
@@ -1,11 +1,11 @@
 package org.esupportail.activbo;
 
 import java.util.Arrays;
-import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.esupportail.activbo.Utils.*;
 
 
